482515d3ce3d93c11efbbecdfcfa97a77af68eab
dlalsrl203 <mingi0429@naver.com>
2020-10-28 14:05:11 +0900

    C==(::^m^)C==

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button roulette1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        roulette1=(Button)findViewById(R.id.roulette1);
+
+        roulette1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Roulette.class);
+                startActivity(intent);
+            }
+        });
     }
 }
