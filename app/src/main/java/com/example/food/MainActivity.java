85b2cef934f6dab4794f351c55ac275807446354
dlalsrl203 <mingi0429@naver.com>
2020-11-28 16:19:02 +0900

    네이버 지도_정보창_정리, 카테고리 연결

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     Button roulette1;
     Button map;
     Button ladder;
-    Button korea, japan;
+    Button korea, japan, chain, western, flour, alone, delivery, asia, fast, latenihgt, cafe, pig,steamedsoup,box,chicken,pizza,all;
 
 
     @Override
@@ -23,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
         ladder=(Button)findViewById(R.id.ladder1);
         korea=(Button)findViewById(R.id.korea);
         japan=(Button)findViewById(R.id.japan);
+        chain=(Button)findViewById(R.id.chain);
+        western=(Button)findViewById(R.id.western);
+        flour=(Button)findViewById(R.id.flour);
+        alone=(Button)findViewById(R.id.alone);
+        delivery=(Button)findViewById(R.id.delivery);
+        asia=(Button)findViewById(R.id.asia);
+        fast=(Button)findViewById(R.id.fast);
+        latenihgt=(Button)findViewById(R.id.latenihgt);
+        cafe=(Button)findViewById(R.id.cafe);
+        pig=(Button)findViewById(R.id.pig);
+        steamedsoup=(Button)findViewById(R.id.steamedsoup);
+        box=(Button)findViewById(R.id.box);
+        chicken=(Button)findViewById(R.id.chicken);
+        pizza=(Button)findViewById(R.id.pizza);
+        all=(Button)findViewById(R.id.all);
+
 
         roulette1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +75,126 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),test.class);
                 intent.putExtra("category"," 일식");
+                startActivity(intent);
+            }
+        });
+        chain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 중식");
+                startActivity(intent);
+            }
+        });
+        western.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 양식");
+                startActivity(intent);
+            }
+        });
+        flour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 분식");
+                startActivity(intent);
+            }
+        });
+        alone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 혼밥");
+                startActivity(intent);
+            }
+        });
+        delivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 배달");
+                startActivity(intent);
+            }
+        });
+        asia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 아시아");
+                startActivity(intent);
+            }
+        });
+        fast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 햄버거");
+                startActivity(intent);
+            }
+        });
+        latenihgt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 야식");
+                startActivity(intent);
+            }
+        });
+        cafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 카페");
+                startActivity(intent);
+            }
+        });
+        pig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 족발");
+                startActivity(intent);
+            }
+        });
+        steamedsoup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 찜");
+                startActivity(intent);
+            }
+        });
+        box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 도시락");
+                startActivity(intent);
+            }
+        });
+        chicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 치킨");
+                startActivity(intent);
+            }
+        });
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 피자");
+                startActivity(intent);
+            }
+        });
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),test.class);
+                intent.putExtra("category"," 식당");
                 startActivity(intent);
             }
         });
