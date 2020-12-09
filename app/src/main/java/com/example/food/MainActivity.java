28ae1629adeb5c917b81a45005e4179123028dda
dlalsrl203 <mingi0429@naver.com>
2020-12-09 16:29:13 +0900

    최종

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton roulette1;
@@ -18,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maintest);
+        setContentView(R.layout.activity_main);
         setTitle("");
         roulette1=(ImageButton)findViewById(R.id.roulette1);
         ladder=(ImageButton)findViewById(R.id.ladder1);
