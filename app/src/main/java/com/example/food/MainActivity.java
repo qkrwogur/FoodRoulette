1f5863cd7777842a0022e01e32b271d3a453d7a8
dlalsrl203 <mingi0429@naver.com>
2020-12-09 16:54:04 +0900

    즐겨찾기 데이터 베이스

---
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -0,0 +1,236 @@
+package com.example.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton roulette1;
+    ImageButton map,roulette2;
+    ImageButton ladder;
+    ImageButton korea, japan, chain, western, flour, asia, fast, cafe, pig,steamedsoup,box,chicken,pizza,all;
+    ImageButton pay,logoutBtn,myinfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("");
+        roulette1=(ImageButton)findViewById(R.id.roulette1);
+        ladder=(ImageButton)findViewById(R.id.ladder1);
+        korea=(ImageButton)findViewById(R.id.korea);
+        japan=(ImageButton)findViewById(R.id.japan);
+        chain=(ImageButton)findViewById(R.id.chain);
+        western=(ImageButton)findViewById(R.id.western);
+        flour=(ImageButton)findViewById(R.id.flour);
+        asia=(ImageButton)findViewById(R.id.asia);
+        fast=(ImageButton)findViewById(R.id.fast);
+        cafe=(ImageButton)findViewById(R.id.cafe);
+        pig=(ImageButton)findViewById(R.id.pig);
+        steamedsoup=(ImageButton)findViewById(R.id.steamedsoup);
+        box=(ImageButton)findViewById(R.id.box);
+        chicken=(ImageButton)findViewById(R.id.chicken);
+        pizza=(ImageButton)findViewById(R.id.pizza);
+        all=(ImageButton)findViewById(R.id.all);
+
+        pay =(ImageButton)findViewById(R.id.pay);
+        logoutBtn=(ImageButton)findViewById(R.id.logoutBtn);
+        myinfo=(ImageButton)findViewById(R.id.myinformation);
+        map=(ImageButton)findViewById(R.id.map);
+        roulette2=(ImageButton)findViewById(R.id.roulette2);
+
+
+        roulette1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),RouletteList.class);
+                startActivity(intent);
+            }
+        });
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 식당");
+                startActivity(intent);
+            }
+        });
+        ladder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),ladder.class);
+                startActivity(intent);
+            }
+        });//
+        korea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 한식");
+                startActivity(intent);
+            }
+        });
+        japan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 일식");
+                startActivity(intent);
+            }
+        });
+        chain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 중식");
+                startActivity(intent);
+            }
+        });
+        western.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 양식");
+                startActivity(intent);
+            }
+        });
+        flour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 분식");
+                startActivity(intent);
+            }
+        });
+
+        asia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 아시아");
+                startActivity(intent);
+            }
+        });
+        fast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 햄버거");
+                startActivity(intent);
+            }
+        });
+
+        cafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 카페");
+                startActivity(intent);
+            }
+        });
+        pig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 족발");
+                startActivity(intent);
+            }
+        });
+        steamedsoup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 찜");
+                startActivity(intent);
+            }
+        });
+        box.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 도시락");
+                startActivity(intent);
+            }
+        });
+        chicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 치킨");
+                startActivity(intent);
+            }
+        });
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 피자");
+                startActivity(intent);
+            }
+        });
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Mappage.class);
+                intent.putExtra("category"," 식당");
+                startActivity(intent);
+            }
+        });
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),pay.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SessionManagement sessionManagement = new SessionManagement(MainActivity.this);
+                sessionManagement.removeSession();
+
+                moveToLogin();
+                finish();
+
+            }
+        });
+
+        myinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),MyInformation.class);
+                startActivity(intent);
+
+            }
+        });
+        roulette2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),RouletteList.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+    private void moveToLogin() {
+        Intent intent = new Intent(MainActivity.this, login.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finish();
+
+    }
+
+}
